4ff239c6bb9562c6ac6a199402f0a5f3bf6ae4be
lyc <31780186+yl3800@users.noreply.github.com>
2018-04-06 00:23:44 -0400

    Update datasets.py

--- datasets.py
+++ datasets.py
@@ -12,54 +12,58 @@ import progressbar
 import sys
 import torchvision.transforms as transforms
 import argparse
-import json
+import json #JavaScript
 
 
 class PartDataset(data.Dataset):
     def __init__(self, root, npoints = 2500, classification = False, class_choice = None, train = True):
         self.npoints = npoints
         self.root = root
-        self.catfile = os.path.join(self.root, 'synsetoffset2category.txt')
+        self.catfile = os.path.join(self.root, 'synsetoffset2category.txt')# 生成绝对路径
         self.cat = {}
 
         self.classification = classification
 
-        with open(self.catfile, 'r') as f:
+        with open(self.catfile, 'r') as f: #打开self.catfile，只读（f=open（‘self.catfile’，‘r’））
             for line in f:
                 ls = line.strip().split()
-                self.cat[ls[0]] = ls[1]
-        #print(self.cat)
-        if not class_choice is  None:
-            self.cat = {k:v for k,v in self.cat.items() if k in class_choice}
+                self.cat[ls[0]] = ls[1]       # 字典生成方式
+        #print(self.cat)                       #print出一个字典 self.cat={'Earphone': '03261776','Motorbike': '03790512,...}
+        
+        
+        if not class_choice is  None:         # 输入一个 lass_choice
+            self.cat = {k:v for k,v in self.cat.items() if k in class_choice}   #=》选定训练对象{'Mug': '03797390'，。。。}
 
         self.meta = {}
-        for item in self.cat:
+        for item in self.cat:  #对于每一种物品
             #print('category', item)
             self.meta[item] = []
-            dir_point = os.path.join(self.root, self.cat[item], 'points')
-            dir_seg = os.path.join(self.root, self.cat[item], 'points_label')
+            dir_point = os.path.join(self.root, self.cat[item], 'points')  # points文件夹（每个chair一个file，存储chair的三维点云坐标）
+            dir_seg = os.path.join(self.root, self.cat[item], 'points_label') #points_label文件夹（每个chair一个file，存储chair上每个的label）
             #print(dir_point, dir_seg)
-            fns = sorted(os.listdir(dir_point))
+            fns = sorted(os.listdir(dir_point))  # os.listdir 列出所有文件
             if train:
-                fns = fns[:int(len(fns) * 0.9)]
+                fns = fns[:int(len(fns) * 0.9)] #前九个椅子作为training data
             else:
-                fns = fns[int(len(fns) * 0.9):]
+                fns = fns[int(len(fns) * 0.9):] #最后1个作为test data
 
             #print(os.path.basename(fns))
-            for fn in fns:
-                token = (os.path.splitext(os.path.basename(fn))[0])
+            for fn in fns:                     #对于每个椅子的pointcloud文件
+                token = (os.path.splitext(os.path.basename(fn))[0]) #token 就是去掉路径，去掉.pts的文件名
                 self.meta[item].append((os.path.join(dir_point, token + '.pts'), os.path.join(dir_seg, token + '.seg')))
-
+                #self.meta['chair']=['/home/.../000001.pts','home/.../00001.seg']
+             
         self.datapath = []
         for item in self.cat:
             for fn in self.meta[item]:
                 self.datapath.append((item, fn[0], fn[1]))
+                # self.datapath=['chair','/home/.../000001.pts','home/.../00001.seg','chair'.......]
 
 
-        self.classes = dict(zip(self.cat, range(len(self.cat))))
+        self.classes = dict(zip(self.cat, range(len(self.cat)))) #{'Mug': 0, 'Chair': 1}
         print(self.classes)
         self.num_seg_classes = 0
-        if not self.classification:
+        if not self.classification: 
             for i in range(len(self.datapath)/50):
                 l = len(np.unique(np.loadtxt(self.datapath[i][-1]).astype(np.uint8)))
                 if l > self.num_seg_classes:
@@ -67,9 +71,9 @@ class PartDataset(data.Dataset):
         #print(self.num_seg_classes)
 
 
-    def __getitem__(self, index):
+    def __getitem__(self, index): # __getitem__,python四种特殊函数之一，例如d=PartDataset（。。。），d[index]，就会调用__getitem__
         fn = self.datapath[index]
-        cls = self.classes[self.datapath[index][0]]
+        cls = self.classes[self.datapath[index][0]]  # cls='chair'
         point_set = np.loadtxt(fn[1]).astype(np.float32)
         seg = np.loadtxt(fn[2]).astype(np.int64)
         #print(point_set.shape, seg.shape)
