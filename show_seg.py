c9021d35933ae3fd217cf5a3d66421eeac509787
lyc <31780186+yl3800@users.noreply.github.com>
2018-04-09 17:01:52 -0400

    Update show_seg.py

--- show_seg.py
+++ show_seg.py
@@ -10,8 +10,8 @@ import torch.nn.parallel
 import torch.backends.cudnn as cudnn
 import torch.optim as optim
 import torch.utils.data
-import torchvision.datasets as dset
-import torchvision.transforms as transforms
+import torchvision.datasets as dset  #没用
+import torchvision.transforms as transforms    #没用
 import torchvision.utils as vutils
 from torch.autograd import Variable
 from datasets import PartDataset
@@ -24,9 +24,8 @@ import matplotlib.pyplot as plt
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument('--model', type=str, default = '',  help='model path')
-parser.add_argument('--idx', type=int, default = 0,   help='model index')
-
+parser.add_argument('--model', type=str, default = '',  help='model path') #训练n个epoch得出的那个模型，后面数字越大越精确
+parser.add_argument('--idx', type=int, default = 0,   help='model index')  #选择testset中的第idx个object做为test对象  
 
 
 opt = parser.parse_args()
@@ -49,7 +48,7 @@ cmap = plt.cm.get_cmap("hsv", 10)
 cmap = np.array([cmap(i) for i in range(10)])[:,:3]
 gt = cmap[seg.numpy() - 1, :]
 
-classifier = PointNetDenseCls(k = 4)
+classifier = PointNetDenseCls(k = 4 )   #K=4，与chair对应
 classifier.load_state_dict(torch.load(opt.model))
 
 point = point.transpose(1,0).contiguous()
